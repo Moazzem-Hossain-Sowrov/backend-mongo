377e9355a497f0737ebf0bc871b8628d6ada74da
Moazzem-Hossain-Sowrov <145192654+Moazzem-Hossain-Sowrov@users.noreply.github.com>
2025-12-07 18:20:25 +0600

    updateData page service

--- index.js
+++ index.js
@@ -63,6 +63,15 @@ async function run() {
       
     })
 
+    app.get('/my-services',async(req,res) =>{
+      const {email} = req.query
+       const query = {email: email}
+       const result = await petServices.find(query).toArray()
+       res.send(result)
+      
+
+    } )
+
 
     await client.db("admin").command({
       ping: 1
